fe799eb4e2e642fc06c40b3fc2d6594f825fc72a
Serega <pavlikovskiy@gmail.com>
2014-04-23 15:28:29 -0600

    move resizeImage2Square to libs

--- samples/coins/libs/resizeImage2Square.m
+++ samples/coins/libs/resizeImage2Square.m
@@ -1,7 +1,10 @@
 function [out] = resizeImage2Square(image, imgW, imgH)
+% Coin images are not square. usualy width = 2*height
+% function assume imgW >= imgH
+% resizeImage2Square makes coin image square (so, it can be displayed with display_network)
+%
 % image imgW*imgH X 1
-
-% out imgW*imgH X 1
+% out   imgW*imgW X 1
 
     img = reshape(image, imgH, imgW);
 
